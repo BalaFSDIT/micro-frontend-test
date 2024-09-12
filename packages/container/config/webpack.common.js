2434b75c00710c5e336a4ba2ec7005f39a45294f
Bala FSDIT <balasureshv@gmail.com>
2024-09-12 15:56:34 +0530

    Add updated webpack config files

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,6 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
         rules : [{
@@ -10,8 +13,11 @@ module.exports = {
                     plugins: ['@babel/plugin-transform-runtime']
                 }
             }
-
-
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 };
--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,14 +1,25 @@
-const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpackCommon = require('./webpack.common');
+const { merge } = require('webpack-merge');
 
-const devConfig = {
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
     mode: 'production',
+    output : {
+        filename: '[name].[contenthash].js'
+    },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${domain}/remoteEntry.js`
+            },
+            shared: packageJson.dependencies
         })
     ]
 };
 
-module.exports = merge(webpackCommon, devConfig);
+module.exports = merge( commonConfig, prodConfig );
