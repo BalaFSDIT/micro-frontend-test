824af02aebe0f9b64d82cf1248ace74fbe3dda39
Bala FSDIT <balasureshv@gmail.com>
2024-09-13 10:24:45 +0530

    Add marketing domain in container

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,7 +4,7 @@ const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.MARKETING_DOMAIN;
 
 const prodConfig = {
     mode: 'production',
