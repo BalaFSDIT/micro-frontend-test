7a12cad6650645059b8e451ee2e7f1e571ecb0cf
Bala FSDIT <balasureshv@gmail.com>
2024-09-13 11:41:44 +0530

    Invalidate the marketing site

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import MarketingApp from './MarketingApp.jsx';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import Header from './Header.js';
 import { BrowserRouter } from 'react-router-dom';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'mrkt-'
+});
+
 function App() {
-    return <div>
+    return <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
             I am a React Container!
             <Header></Header>
             <MarketingApp></MarketingApp>
         </BrowserRouter>
-    </div>
+    </StylesProvider>
 };
 
 export default App;
